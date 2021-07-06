2b0adefaaeb69320af6c09820fa0031a9e8c713a
Jesper Kallunki <jesper.kallunki@outlook.com>
2021-07-06 20:07:05 +0300

    commit

--- set-1/challenge-5/challenge-5.php
+++ set-1/challenge-5/challenge-5.php
@@ -1,3 +1,13 @@
 <?php
 
+$input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
 
+$key = "ICE";
+
+$key = substr(str_repeat($key, strlen($input)), 0, strlen($input));
+
+$output = $input ^ $key;
+
+$output = bin2hex($output);
+
+echo $output;
